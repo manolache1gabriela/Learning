aa9bf265fa095e5b56d059b1543f741c61b762fa
Manolache Gabriela <manolachegabriela23@gmail.com>
2023-08-28 19:06:03 +0000

    ep 7

--- lc-react/src/components/App.jsx
+++ lc-react/src/components/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import NoTodos from './NoTodos';
 import TodoForm from './TodoForm';
-import TodoList from './TodoList'
+import TodoList from './TodoList';
 import '../reset.css';
 import '../App.css';
+
+
 
 function App() {
   const [todos, setTodos] = useState([
@@ -97,6 +99,36 @@ const [idForTodo, setIdForTodo] = useState(4);
     setTodos(updatedTodos);
   }
 
+
+  function remaining(){
+    return todos.filter(todo => !todo.isComplete).length;
+  }
+
+  function clearCompleted(){
+    setTodos([...todos].filter(todo => !todo.isComplete));
+  }
+
+    function completeAllTodos() {
+    const updatedTodos = todos.map(todo => {
+      todo.isComplete = true;
+
+      return todo;
+    });
+
+    setTodos(updatedTodos);
+  }
+
+
+  function todosFiltered(filter){
+    if (filter === "all"){
+      return todos;
+    } else if (filter === "active"){
+      return todos.filter(todo => !todo.isComplete)
+    } else if (filter === "completed"){ 
+      return todos.filter(todo => todo.isComplete)
+    }
+  }
+
   return (
     <div className="todo-app-container">
       <div className="todo-app">
@@ -112,6 +144,10 @@ const [idForTodo, setIdForTodo] = useState(4);
         updateTodo={updateTodo}
         cancelEdit={cancelEdit}
         deleteTodo={deleteTodo}
+        remaining={remaining}
+        clearCompleted={clearCompleted}
+        completeAllTodos={completeAllTodos}
+        todosFiltered={todosFiltered}
         />
         ) : (
           <NoTodos/>
