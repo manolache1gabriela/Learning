a5f20e5c4ce0cca31505eae783fba7970acf00e9
Gabriela <manolachegabriela23@gmail.com>
2023-09-19 17:17:22 +0300

    episode 14

--- new-app/my-app/src/Joke.jsx
+++ new-app/my-app/src/Joke.jsx
@@ -1,29 +1,19 @@
 import React, { useEffect, useState } from 'react';
+import useFetch from './useFetch';
 
 export default function Joke() {
-  const [joke, setJoke] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
-    const [errorMessage, setErrorMessage] = useState(null);
-    
-    useEffect(()=>{
-        fetch('https://official-joke-api.appspot.com/jokes/random')
-            .then(response => response.json())
-            .then(result => {
-                setIsLoading(false)
-                setJoke(result.setup + ' ' + result.punchline)
-            })
-            .catch(error => {
-                setIsLoading(false);
-                setErrorMessage('There was an error')
-            })
-    }, [])
+  const {
+    data: joke,
+    isLoading,
+    errorMessage,
+  } = useFetch('https://official-joke-api.appspot.com/jokes/random');
 
   return (
   <div>
     <h2>Reddit API</h2>
     {isLoading && <div>Loading...</div>}
     {joke && (
-        <div>{joke}</div>
+        <div>{joke.setup} {joke.punchline}</div>
     )}
         {errorMessage && <div>{errorMessage}</div>}
   </div>
