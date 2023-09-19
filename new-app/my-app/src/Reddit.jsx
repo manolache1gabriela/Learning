58f7554df45a6c42f7d898635a321df673cb646b
Gabriela <manolachegabriela23@gmail.com>
2023-09-19 18:29:13 +0300

    episode 15

--- new-app/my-app/src/Reddit.jsx
+++ new-app/my-app/src/Reddit.jsx
@@ -1,26 +1,48 @@
 import React from 'react';
-import useFetch from './useFetch';
+// import useFetch from './useFetch';
+import { useQuery } from '@tanstack/react-query';
 
 export default function Reddit() {
+ 
+  // const {
+  //   data: posts,
+  //   isLoading,
+  //   errorMessage,
+  // } = useFetch('https://www.reddit.com/r/aww.json');
+
   const {
     data: posts,
     isLoading,
-    errorMessage,
-  } = useFetch('https://www.reddit.com/r/aww.json');
+    isError,
+    error,
+    isSuccess,
+  } = useQuery(['posts'], fetchPosts, {
+    retry: false
+  });
+
+  function fetchPosts() {
+    return fetch('https://www.reddit.com/r/aww.json').then(response =>
+      response.json()
+    );
+  }
+
+
   return (
-  <div>
-    <h2>Reddit API</h2>
-    {isLoading && <div>Loading...</div>}
-    {posts && (
-        <ul>
+    <div>
+      <h2>Reddit API</h2>
+      {isLoading && <div>Loading...</div>}
+      {isSuccess && (
+          <ul>
             {posts.data.children.map(post => (
-            <li key={post.data.id}>
-                <a href={`https://reddit.com${post.data.permalink}`}>{post.data.title}</a>
-            </li>
-    ))}
-        </ul>
-    )}
-        {errorMessage && <div>{errorMessage}</div>}
-  </div>
+              <li key={post.data.id}>
+                  <a href={`https://reddit.com${post.data.permalink}`}>
+                    {post.data.title}
+                  </a>
+              </li>
+            ))}
+          </ul>
+        )}
+      {isError && <div>{error.message}</div>}
+    </div>
   );
 }
